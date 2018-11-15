cfc668bfe3fe54f9b17b7b6a4515c5998a1a12db
gavinconran <gtconran@gmail.com>
2018-11-15 15:54:17 +0000

    generic simulation

--- DSP/CodeForImages/FourierSeries_HeatEquation.py
+++ DSP/CodeForImages/FourierSeries_HeatEquation.py
@@ -23,7 +23,7 @@ def Dn_coeff(L, n, x):
     '''
     if (n==1): # (when m == n)
         return 1.0
-    else: # (ehen m != n)
+    else: # (when m != n)
         return 0.0
 
 def heat(n, x, alpha, t, L):
@@ -35,16 +35,20 @@ def heat(n, x, alpha, t, L):
     L is the length of the rod
     return u(x, t)
     '''
-    return Dn_coeff(L, n, x) * np.sin(np.pi*x/L) * decay(n, alpha, t, L) 
+    nn = np.arange(1, n+1)
+    return np.sum([Dn_coeff(L, n, x) * np.sin(np.pi*x/L) * decay(n, alpha, t, L) for n in nn]) 
 
 # set parameters
 alpha = 1.22*10**(-3) # thermal diffusivity
 L = 1.0 # length of rod
 u_max = 100.0 # initial max. temp.
 
-dx = 0.01
+dx = 0.01 # distance step
 xx= np.arange(0.00, L+dx, dx)   # distance values
-tt = np.arange(0, 201, 40)  # time values
+
+T = 200 # duration of simulation
+dt = 40 # time step
+tt = np.arange(0, T+dt, dt)  # time values
 
 # plot initial conditions
 ic = u_max*np.sin(xx *np.pi/L)
@@ -61,16 +65,16 @@ plt.show()
 
 # run simulation
 n = 1 # can go to infinity
-simulation1 = [[u_max * heat(n, x, alpha, t, L) for x in xx] for t in tt]
+simulation = [[u_max * heat(n, x, alpha, t, L) for x in xx] for t in tt]
 
 # plot results
 plt.figure(2)
 plt.ylim(0, 110)
-plt.plot(xx, simulation1[0], 'b', label='t = 0')
-plt.plot(xx, simulation1[1], 'r', label='t = 1')
-plt.plot(xx, simulation1[2], 'g', label='t = 2')
-plt.plot(xx, simulation1[3], 'm', label='t = 3')
-plt.plot(xx, simulation1[4], 'c', label='t = 4')
+plt.plot(xx, simulation[0], 'b', label='t = 0')
+plt.plot(xx, simulation[1], 'r', label='t = 1')
+plt.plot(xx, simulation[2], 'g', label='t = 2')
+plt.plot(xx, simulation[3], 'm', label='t = 3')
+plt.plot(xx, simulation[4], 'c', label='t = 4')
 plt.legend()
 plt.title("Solution to Heat Equation over Time"
           "\n"
